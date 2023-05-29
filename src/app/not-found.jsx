dc1aea221f61449ae5882df6328bda53879d4581
Junaid-Z <jzafar828@gmail.com>
2023-05-29 17:57:30 +0500

    Unnecessary files removed

--- src/app/not-found.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Error = () => {
-    return (
-        <div>Custom 404</div>
-    )
-}
-
-export default Error
--- src/app/test/@stream/error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const error = () => {
-  return (
-    <div>the stream broke</div>
-  )
-}
-
-export default error
